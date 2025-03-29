487454d41bef89099b612292c5e4be32673b8946
Sahil Sachan <sahilsachan2727@gmail.com>
2025-03-29 17:10:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function appendCharacter(character) {
+    document.getElementById("display").value += character;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch {
+        document.getElementById("display").value = "Error";
+    }
+}
